ea94cd746c8b4335bb23bdb5885960843c6c63f2
Samantha Balgobin <sbalgobin94@gmail.com>
2020-07-21 20:44:38 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,7 +36,8 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+    
+    programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,8 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
@@ -81,6 +84,9 @@ def adding_matz
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+    programmer_hash
 
 end
 
@@ -103,6 +109,9 @@ def changing_alan
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+    programmer_hash
 
 end
 
@@ -125,5 +134,9 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+    programmer_hash
 
 end
+
